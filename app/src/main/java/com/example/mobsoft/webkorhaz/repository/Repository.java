769c38b210c64ecd3607521c09981bb4e8e1ce74
ApatiAti <tntiti@hotmail.com>
2017-04-03 23:12:38 +0200

    adding model and repository package

---
+++ app/src/main/java/com/example/mobsoft/webkorhaz/repository/Repository.java
@@ -0,0 +1,25 @@
+package com.example.mobsoft.webkorhaz.repository;
+
+import android.content.Context;
+
+import com.example.mobsoft.webkorhaz.model.Todo;
+
+import java.util.List;
+
+
+public interface Repository {
+
+    void open(Context context);
+
+    void close();
+
+    List<Todo> getFavourites();
+
+    void saveFavourite(Todo todo);
+
+    void updateFavourites(List<Todo> todos);
+
+    void removeFavourite(Todo todo);
+
+    boolean isInDB(Todo todo);
+}
