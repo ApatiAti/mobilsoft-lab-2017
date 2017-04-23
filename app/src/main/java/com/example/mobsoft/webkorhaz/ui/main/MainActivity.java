266d42b370f7dbb4aef1cbcb277aa46e9fff76e0
ApatiAti <tntiti@hotmail.com>
2017-04-23 23:34:31 +0200

    Fix navigation issue

--- app/src/main/java/com/example/mobsoft/webkorhaz/ui/main/MainActivity.java
+++ app/src/main/java/com/example/mobsoft/webkorhaz/ui/main/MainActivity.java
@@ -95,8 +95,9 @@ public class MainActivity extends AppCompatActivity implements MainScreen {
     private void navigateToNavigationActivity() {
         Intent intent = new Intent(MainActivity.this, NavigationActivity.class);
         // Hogy ne tudjunk vissza navigálni
-        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         startActivity(intent);
+//        finish();
     }
 
     /**
